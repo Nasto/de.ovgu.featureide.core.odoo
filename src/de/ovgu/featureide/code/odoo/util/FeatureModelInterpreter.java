4f980dc903be9a2290ff31a66b6a6d2b1a8e6be8
Jens <jens.wiemann@hotmail.com>
2015-12-09 13:15:46 +0100

    FolderName Parsing angefangen

--- src/de/ovgu/featureide/code/odoo/util/FeatureModelInterpreter.java
+++ src/de/ovgu/featureide/code/odoo/util/FeatureModelInterpreter.java
@@ -3,18 +3,65 @@ package de.ovgu.featureide.code.odoo.util;
 import java.io.File;
 import java.util.ArrayList;
 
+import de.ovgu.featureide.fm.core.*;
+
 public class FeatureModelInterpreter {
 	
 	private static void parseFolderStructure(File[] folders){
-		ArrayList<String> tempFolderNames = new ArrayList<String>();		
+		ArrayList<String> folderNames = new ArrayList<String>();		
 		for (File folder : folders){
-			tempFolderNames.add(folder.getName());			
+			folderNames.add(folder.getName());			
 		}
-		String[] folderNames = new String[tempFolderNames.size()];
-		tempFolderNames.toArray(folderNames);
+		
+		folderNames = orderFolderNames(folderNames);
 		
 		for (String folder : folderNames)
-		System.out.println(folder);		
+			System.out.println(folder);		
+		
+		FeatureModel fm = new FeatureModel();
+		Feature f = new Feature(fm,"Odoo");
+		fm.setRoot(f);
+		Feature child = new Feature(fm,"kind");
+		fm.addFeature(child);
+		f.addChild(child);
+		System.out.println(fm.toString());
+		
+	}
+	
+	private static void addFolderNameToFM(FeatureModel fm, String FolderName){
+		int interleavingDegree = FolderName.length() - FolderName.replace("_", "").length();
+		if(interleavingDegree == 0){
+			Feature root = fm.getRoot();
+			Feature newFeature = new Feature(fm,FolderName);
+			fm.addFeature(newFeature);
+			root.addChild(newFeature);
+		}
+		
+	}
+	
+	private static void addFolderNameToFMRec(FeatureModel fm, String FolderName){
+		//Hier bin ich gerade..
+	}
+	
+	private static ArrayList<String> orderFolderNames(ArrayList<String> folderNames){
+		ArrayList<String> result = new ArrayList<>();
+		if(folderNames.size()==0) return result;
+		int i = 0;
+		while(result.size() != folderNames.size()){			
+			result.addAll(getStringsContaining(folderNames,"_",i++));
+		}
+		return result;
+	}
+	
+	private static ArrayList<String> getStringsContaining(ArrayList<String> totalStrings, String substring, int amount){
+		ArrayList<String> result = new ArrayList<>();
+		for (String string : totalStrings){
+			int occurency = string.length() - string.replace(substring, "").length();
+			if(occurency == amount){
+				result.add(string);
+			}
+		}
+		return result;
 	}
 	
 	public static String createFeatureModel(){
